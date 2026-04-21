3e1d5fd087a9bc60a2623e3ea0a0a077ddf62a60
Brijesh K Dhaker <brijeshdhaker@gmail.com>
2026-04-21 21:31:44 +0530

    2026-04-21 Update folder structures.

--- src/main/py/com/example/agentic/tools/config.py
+++ src/main/py/com/example/agentic/tools/config.py
@@ -5,7 +5,8 @@ _tool_config = dict(
         provider="ollama", # or google, openai, anthropic, llama2, ...
         config=dict(
             model="llama3.2:1b-instruct-q8_0",
-            # temperature=0.5,
+            # num_predict=256,
+            # temperature=0.7,
             # top_p=1,
             # stream=true,
         ),
@@ -16,8 +17,7 @@ _tool_config = dict(
     ),
     embedding_model= dict(
         provider="openai",
-        config=dict(model= "nomic-embed-text", api_key="", platform_url="http://localhost:11434/v1"
-        )
+        config=dict(model= "nomic-embed-text", api_key="", platform_url="http://localhost:11434/v1")
     ),
     vectordb=dict(
         provider="chromadb",
@@ -42,8 +42,7 @@ _rag_tool_config = dict(
     ),
     embedding_model= dict(
         provider="openai",
-        config=dict(model= "nomic-embed-text", api_key="", platform_url="http://localhost:11434/v1"
-        )
+        config=dict(model= "nomic-embed-text", api_key="", platform_url="http://localhost:11434/v1")
     ),
     vectordb=dict(
         provider="chromadb",
