8baf032064c4094f39cb375bdcfcd846ddc5b5d4
Brijesh K Dhaker <brijeshdhaker@gmail.com>
2026-04-29 08:20:09 +0530

    2026-04-29 Update model name

--- src/main/py/com/example/agentic/agent/LLMManager.py
+++ src/main/py/com/example/agentic/agent/LLMManager.py
@@ -2,52 +2,31 @@
 import os
 import getpass
 from dotenv import load_dotenv
-from emoji import config
 from langchain.agents import create_agent
 from langchain.chat_models import init_chat_model
 from dotenv import load_dotenv
 from crewai import LLM
 
 #
-# "groq:llama3–8b-8192"
-# "groq:llama3-70b-8192" 
-# "groq:openai/gpt-oss-20b"
-# "groq:llama-3.1-70b-versatile"
-
+# llama3.2:3b-instruct-q8_0
+# nomic-embed-text:latest
+#
 class LLMManager:
     """
     """
-    __agent = None
-    __model = None
-
     @classmethod
     def get_agent(cls):
         load_dotenv()
-        #
-        if not os.environ.get("OPENAI_API_KEY"):
-            os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter your OpenAI API key : ")
-
         # Initialize models
-        if not cls.__agent :
-            cls.__agent = create_agent(
-                model="groq:openai/gpt-oss-20b",
-                system_prompt="You are a helpful assistant."
-            )
-
-        return cls.__agent
+        return create_agent(
+            model=f"groq:{os.environ["GROQ_MODEL_NAME"]}",
+            system_prompt="You are a helpful assistant."
+        )
     
     @classmethod
     def get_model(cls):
         # Initialize models
-        if not cls.__model :
-            cls.__model = init_chat_model("groq:openai/gpt-oss-20b")
-            #self.llm = ChatOpenAI(
-            #    model="openai/gpt-oss-20b", 
-            #    api_key=os.getenv("OPENAI_API_KEY"),
-            #    base_url="https://api.groq.com/openai/v1",
-            #    temperature=0
-            #)
-        return cls.__model
+        return init_chat_model(f"groq:{os.environ["GROQ_MODEL_NAME"]}")
     
     #
     @classmethod
@@ -57,9 +36,9 @@ class LLMManager:
         if type == 'ollama' :
             # LLM setup using litellm additional_params={"num_ctx":16384},
             return LLM(
-                model="ollama/llama3.2:latest", 
+                model=f"ollama/{os.environ["OLLAMA_MODEL"]}", 
                 base_url="http://localhost:11434", 
-                temperature=0.4,     # Controls randomness
+                temperature=0.3,     # Controls randomness os.environ["OLLAMA_MODEL"]
                 #seed=21,             # Ensures consistent outputs
                 #max_tokens=4096,     # Limits response length
                 #top_p=0.9            # Controls the diversity of the response
@@ -67,19 +46,16 @@ class LLMManager:
         # Groq llm client
         if type == 'groq' :
             return LLM(
-                model="groq/openai/gpt-oss-20b", 
+                model=f"groq/{os.environ["GROQ_MODEL_NAME"]}", 
                 base_url="https://api.groq.com/openai/v1"
             )
         # OpenAI llm client
         if type == 'openai' or type == 'hf':
-            #
-            if not os.environ.get("OPENAI_API_KEY"):
-                os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter your OpenAI API key: ")
             return LLM(
-                model="openai/llama3.2:latest", 
+                model=f"openai/{os.environ["OPENAI_MODEL_NAME"]}", 
                 base_url="http://localhost:11434/v1",
-                temperature=0.2,     # Controls randomness
-                seed=42,             # Ensures consistent outputs
-                max_tokens=4096,     # Limits response length
-                top_p=0.9            # Controls the diversity of the response
+                temperature=0.3,     # Controls randomness
+                #seed=42,             # Ensures consistent outputs
+                #max_tokens=4096,     # Limits response length
+                #top_p=0.9            # Controls the diversity of the response
             )
